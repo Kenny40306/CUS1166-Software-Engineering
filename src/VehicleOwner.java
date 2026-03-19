3b907d2beeecd0099f541bcc2f6f3138c4697c5c
Avneet Kaur <avneet.kaur22@my.stjohns.edu>
2026-03-19 15:45:50 -0400

    Implemented VehicleOwner class with attributes and methods

--- src/VehicleOwner.java
+++ src/VehicleOwner.java
@@ -13,11 +13,40 @@ public class VehicleOwner {
 	protected String residencyTime;
 	protected List<Vehicle> vehicleSubmitted;
 	
+	
 	//Methods
 	public VehicleOwner(String ownerID, String ownerName, String residencyTime) {
 		this.ownerID = ownerID;
 		this.ownerName = ownerName;
 		this.residencyTime = residencyTime;
 		this.vehicleSubmitted = new ArrayList<>();
+	}
+	//method to add the vehicle
+	public void addVehicle(Vehicle vehicle) {
+		vehicleSubmitted.add(vehicle);	
+	}
+	//method to view vehicles
+	public List<Vehicle> getVehciles() {
+		return vehicleSubmitted;
+	}
+	//method to show the owner info
+	public String displayOwnerInfo() {
+		return "Owner ID: " + ownerID +
+				", Name: " +ownerName +
+				", Residency: " + residencyTime;
+	}
+	//getters
+	public String getOwnerID() {
+		return ownerID;
+	}
+	public String getOwnerName() {
+		return ownerName;
+	}
+	public String getResidencyTime() {
+		return residencyTime;
+	}
+	//setter
+	public void setResidencyTime(String residencyTime) {
+		this.residencyTime = residencyTime;
 	}
 }
