424e10bb593f40ba495294a0dddd5fe1cf63ffcb
Avneet Kaur <avneet.kaur22@my.stjohns.edu>
2026-03-22 23:25:09 -0400

    Added comments to VehicleOwner

--- src/VehicleOwner.java
+++ src/VehicleOwner.java
@@ -3,47 +3,54 @@ import java.util.List;
 
 /*=====================
 Class Vehicle Owner Logic - Avneet
+class represents a vehicle owner in the system
+stores owner info and the list of vehicles submitted by the owner
 ======================*/
 
 //----M4 Implementation: 
-public class VehicleOwner extends User {
+public class VehicleOwner {
 
 	//Protected Attributes based on user class inheritance to access these fields
-	 private String residencyTime;
-	    private List<Vehicle> vehicleSubmitted;
+	protected String ownerID;//unique ID for the vehicle owner
+	protected String ownerName;//name of the vehicle owner
+	protected String residencyTime;//how long the vehicle will stay
+	protected List<Vehicle> vehicleSubmitted;// list of vehicles submitted
 	
 	//Contructor:
-	    public VehicleOwner(String userID, String userName, String email, String password, String residencyTime) {
-	        super(userID, userName, email, "VehicleOwner", password);
-	        this.residencyTime = residencyTime;
-	        this.vehicleSubmitted = new ArrayList<>();
-	    }
+	//initializes the vehicle owner object with ID, name, and residency time
+	//also creates an empty list to store vehicles
+	public VehicleOwner(String ownerID, String ownerName, String residencyTime) {
+		this.ownerID = ownerID;
+		this.ownerName = ownerName;
+		this.residencyTime = residencyTime;
+		this.vehicleSubmitted = new ArrayList<>();
+	}
 	
 	//method to add the vehicle
-	    public void addVehicle(Vehicle vehicle) {
-	        vehicleSubmitted.add(vehicle);
-	    }
-		//method to view vehicles
-	    public List<Vehicle> getVehicles() {
-	        return vehicleSubmitted;
-	    }
-	    //method to show the owner info
-	    public String displayOwnerInfo() {
-	        return "Owner ID: " + userID +
-	               ", Name: " + userName +
-	               ", Residency: " + residencyTime;
-	    }
+		public void addVehicle(Vehicle vehicle) {
+			vehicleSubmitted.add(vehicle);	
+		}
+		//method to view vehicles from the list submitted by the owner
+		public List<Vehicle> getVehicles() {
+			return vehicleSubmitted;
+		}
+		//method to show the owner info
+		public String displayOwnerInfo() {
+			return "Owner ID: " + ownerID +
+					", Name: " +ownerName +
+					", Residency: " + residencyTime;
+		}
 		//getters
 		public String getOwnerID() {
-			return userID;
+			return ownerID;
 		}
 		public String getOwnerName() {
-			return userName;
+			return ownerName;
 		}
 		public String getResidencyTime() {
 			return residencyTime;
 		}
-		//setter
+		//setter method to update residency time if needed
 		public void setResidencyTime(String residencyTime) {
 			this.residencyTime = residencyTime;
 		}
