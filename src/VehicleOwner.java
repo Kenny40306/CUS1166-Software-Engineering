d75e1f9c23b4890f8aeddc91ee56c49a6afb82be
JadenGIT2IT <159954379+JadenGIT2IT@users.noreply.github.com>
2026-03-22 22:29:38 -0400

    Refactor VehicleOwner class to extend User class

--- src/VehicleOwner.java
+++ src/VehicleOwner.java
@@ -6,42 +6,39 @@ Class Vehicle Owner Logic - Avneet
 ======================*/
 
 //----M4 Implementation: 
-public class VehicleOwner {
+public class VehicleOwner extends User {
 
 	//Protected Attributes based on user class inheritance to access these fields
-	protected String ownerID;
-	protected String ownerName;
-	protected String residencyTime;
-	protected List<Vehicle> vehicleSubmitted;
+	 private String residencyTime;
+	    private List<Vehicle> vehicleSubmitted;
 	
 	//Contructor:
-	public VehicleOwner(String ownerID, String ownerName, String residencyTime) {
-		this.ownerID = ownerID;
-		this.ownerName = ownerName;
-		this.residencyTime = residencyTime;
-		this.vehicleSubmitted = new ArrayList<>();
-	}
+	    public VehicleOwner(String userID, String userName, String email, String password, String residencyTime) {
+	        super(userID, userName, email, "VehicleOwner", password);
+	        this.residencyTime = residencyTime;
+	        this.vehicleSubmitted = new ArrayList<>();
+	    }
 	
 	//method to add the vehicle
-		public void addVehicle(Vehicle vehicle) {
-			vehicleSubmitted.add(vehicle);	
-		}
+	    public void addVehicle(Vehicle vehicle) {
+	        vehicleSubmitted.add(vehicle);
+	    }
 		//method to view vehicles
-		public List<Vehicle> getVehciles() {
-			return vehicleSubmitted;
-		}
-		//method to show the owner info
-		public String displayOwnerInfo() {
-			return "Owner ID: " + ownerID +
-					", Name: " +ownerName +
-					", Residency: " + residencyTime;
-		}
+	    public List<Vehicle> getVehicles() {
+	        return vehicleSubmitted;
+	    }
+	    //method to show the owner info
+	    public String displayOwnerInfo() {
+	        return "Owner ID: " + userID +
+	               ", Name: " + userName +
+	               ", Residency: " + residencyTime;
+	    }
 		//getters
 		public String getOwnerID() {
-			return ownerID;
+			return userID;
 		}
 		public String getOwnerName() {
-			return ownerName;
+			return userName;
 		}
 		public String getResidencyTime() {
 			return residencyTime;
